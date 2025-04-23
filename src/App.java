998c5488a239c0497046bf7654dd48c63e3b257e
mohamed-elmsallati <mohammed.elmsallaty@gmail.com>
2025-04-23 21:24:00 +0200

    Initialize Java project with basic array sum functionality and configuration

---
+++ src/App.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args){
+        Scanner Console_Scanner = new Scanner(System.in);
+        System.out.println("enter arrays size : ");
+        int array_size = Console_Scanner.nextInt();
+        System.out.println("enter first array elements: ");
+        int[] first_array = Create_Array(array_size);
+        System.out.println("enter second array elements: ");
+        int[] second_array = Create_Array(array_size);
+
+        int[] sum = new int[array_size];
+        for (int i=0; i<array_size; i++){
+            sum[i] = first_array[i] + second_array[i];
+        }  
+
+        for (int i = 0; i < sum.length; i++) {
+            System.out.println(sum[i]);
+        }
+    }
+    public static int[] Create_Array(int array_size){
+        Scanner Console_Scanner = new Scanner(System.in);
+        int[] temp = new int[array_size];
+        for (int i =0; i < array_size;i++){
+            System.out.print("enter next element: ");
+            temp[i] = Console_Scanner.nextInt();
+        }
+        return temp;
+    }
+}
